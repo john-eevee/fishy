620b038e27e00b6cdae5d6432bf99ebe083a171b
John <5256711+john-eevee@users.noreply.github.com>
2025-12-22 17:41:34 -0300

    feat: support args as path to search, remove comments

--- functions/proj.fish
+++ functions/proj.fish
@@ -1,10 +1,12 @@
 function proj --description "Open a project in the current editor"
-    # 1. SETTINGS
-    # Where do you keep your projects? Add more paths here if needed.
-    # We look for folders in ~/Projects, ~/dev, ~/workspace, or just subfolders of Home.
-    set -l search_paths ~/Projects ~/dev ~/code ~/workspace
 
-    # 2. DEFINING COLORS (Catppuccin Mocha)
+    set -l search_paths $args
+
+    if test (count $search_paths) -eq 0
+        set -l search_paths ~/Projects ~/dev ~/code ~/workspace
+    end
+
+    # Catppuccin Mocha color palette
     set -l color00 '#1e1e2e' # Base
     set -l color01 '#181825' # Mantle
     set -l color02 '#313244' # Surface0
@@ -22,9 +24,6 @@ function proj --description "Open a project in the current editor"
     set -l color0E '#cba6f7' # Mauve
     set -l color0F '#f2cdcd' # Flamingo
 
-    # 3. FZF OPTIONS
-    # - Layout: Reverse (top-down) with a border
-    # - Preview: Uses 'eza' to show a tree with icons and git status
     set -l fzf_opts \
     --reverse \
     --border="rounded" \
@@ -38,7 +37,6 @@ function proj --description "Open a project in the current editor"
     --preview="eza --tree --level=2 --icons --git --group-directories-first --color=always {1}" \
     --preview-window="right:60%:border-left"
 
-    # Check which search paths actually exist to avoid errors
     set -l valid_paths
     for path in $search_paths
         if test -d $path
@@ -46,28 +44,20 @@ function proj --description "Open a project in the current editor"
         end
     end
 
-    # If no project folders exist, fallback to Home (depth 1)
     if test (count $valid_paths) -eq 0
         set valid_paths $HOME
     end
 
-    # Run the search
     set -l result (fd --type d --hidden --glob ".git" $valid_paths --exec dirname | fzf $fzf_opts)
 
     if test $EDITOR = ""
         set EDITOR "code"  # Default to VSCode if EDITOR is not set
     end
-    # 5. ACTION
+
     if test -n "$result"
-        # Navigate to the directory
         cd $result
-
         # 'mise' is hooked into fish, so the 'cd' above automatically triggers the env switch.
-
-        # Open EDITOR
         $EDITOR .
-
-        # Clear screen and print success
         commandline -f repaint
         echo -e "\n\033[0;32mOpened project: $result\033[0m"
     end
