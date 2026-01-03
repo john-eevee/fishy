d3d41029242a8635187a979c2b23829fdff0015c
John <5256711+john-eevee@users.noreply.github.com>
2026-01-03 19:18:39 -0300

    feat: forward args to underlying extractor

--- functions/extract.fish
+++ functions/extract.fish
@@ -1,30 +1,44 @@
-function extract --description "Expand various archive formats"
-    if test -f $argv[1]
-        switch $argv[1]
+function extract --description "Expand archive formats, passing extra args to the tool"
+    set -l file $argv[1]
+
+    # Capture all remaining arguments to pass to the extractor
+    set -l opts $argv[2..-1]
+
+    if test -f "$file"
+        switch "$file"
             case '*.tar.bz2'
-                tar xjf $argv[1]
+                # tar supports trailing options (e.g. -C /path)
+                tar xjf "$file" $opts
             case '*.tar.gz'
-                tar xzf $argv[1]
+                tar xzf "$file" $opts
             case '*.bz2'
-                bunzip2 $argv[1]
+                # bunzip2 usually works on file in place, opts might be --force etc
+                bunzip2 "$file" $opts
             case '*.rar'
-                unrar x $argv[1]
+                unrar x "$file" $opts
             case '*.gz'
-                gunzip $argv[1]
+                gunzip "$file" $opts
             case '*.tar'
-                tar xf $argv[1]
+                tar xf "$file" $opts
             case '*.tbz2'
-                tar xjf $argv[1]
+                tar xjf "$file" $opts
             case '*.tgz'
-                tar xzf $argv[1]
+                tar xzf "$file" $opts
             case '*.zip'
-                unzip $argv[1]
+                # unzip uses -d for destination
+                unzip "$file" $opts
             case '*.Z'
-                uncompress $argv[1]
+                uncompress "$file" $opts
+            case '*.7z'
+                7z x "$file" $opts
+            case '*.xz'
+                unxz "$file" $opts
             case '*'
-                echo "'$argv[1]' cannot be extracted via extract()"
+                echo "'$file' cannot be extracted via extract()"
+                return 1
         end
     else
-        echo "'$argv[1]' is not a valid file"
+        echo "'$file' is not a valid file"
+        return 1
     end
 end
